8589952d36022b3858c95aa3316c4f7cb50cdbff
mcsubash <103946206+mcsubash@users.noreply.github.com>
2022-12-14 15:58:10 +0530

    Update Hooks.java

--- src/main/java/org/baseClass/Hooks.java
+++ src/main/java/org/baseClass/Hooks.java
@@ -2,7 +2,7 @@ package org.baseClass;
 
 import java.io.File;
 import java.io.IOException;
-
+//to check git
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
